9661a529c07146d3d0ee068e80b7016f38395423
Elsa Rhudye Mangoye <diamant.rhudye@gmail.com>
2021-01-22 18:41:24 +0100

    Car list component

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground} from 'react-native';
-import CarItem from './components/CarItem';
+import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <CarItem
-        name={"Model 3"}
-        tagline={"Order Online for"}
-        taglineCTA={"Touchless Delevery"}
-        image={require('./assets/images/Model3.jpeg')}
-      
-      />
+      <CarsList />
 
       
       <StatusBar style="auto" />
